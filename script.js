86c8985620cb11c77280d54da1253256cea72e17
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-01-22 16:51:08 +0600

    print xyz if divisible

--- script.js
+++ script.js
@@ -26,5 +26,20 @@ a = b;
 b = c;
 console.log(a, b);
 
-// ===================== Problem 03 :
+// ===================== Problem 03 : Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+
+let number = 1;
+
+for (number = 1; number <= 100; number++) {
+    if (number % 3 === 0 && number % 5 !== 0) {
+        console.log("Fizz");
+    } else if (number % 5 === 0 && number % 3 !== 0) {
+        console.log("Buzz");
+    } else if (number % 3 === 0 && number % 5 === 0) {
+        console.log("FizzBuzz")
+    } else {
+        console.log(number)
+    }
+}
+
 
