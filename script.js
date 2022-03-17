2d9a376ee7372ee5a3768af6b26e4375c7f89601
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-03-17 11:09:40 +0600

    problem 26 (Unsolved)

--- script.js
+++ script.js
@@ -449,23 +449,59 @@
 
 //** ===================== Problem 25 : The sum of the squares of the first ten natural numbers is 385,The square of the sum of the first ten natural numbers is 3025, Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 2640. Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-let totalSumOfSquares = 0;
-let totalSquareOfSum = 0;
-const sumOfSquares = (n) => {
-    for (let i = 1; i <= n; i++) {
-        totalSumOfSquares += i ** 2
+// let totalSumOfSquares = 0;
+// let totalSquareOfSum = 0;
+// const sumOfSquares = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         totalSumOfSquares += i ** 2
+//     }
+// }
+// sumOfSquares(100)
+
+// const squareOfSum = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         totalSquareOfSum += i;
+//     }
+//     totalSquareOfSum = totalSquareOfSum ** 2;
+// }
+
+// squareOfSum(100)
+
+// let difference = totalSquareOfSum - totalSumOfSquares;
+// console.log(difference)
+
+
+//** ===================== Problem 26 :The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+const digitForTest = `7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450`
+//* ---> Find the thirteen adjacent digits in the 1000 - digit number that have the greatest product.What is the value of this product ?
+
+let largestProduct = 1;
+const adjacentDigits = (n) => {
+    const array = n.toString().split("");
+
+    let productSample = 1;
+    let start = 0;
+    let range = 4;
+    let stopCondition = array.length;
+
+
+    for (let i = 1; i < stopCondition; i++) {
+        if (range >= array.length) {
+            stopCondition = 0;
+            range = 0;
+        }
+        for (let start = 0; start < range; start++) {
+            productSample *= Number(array[start]);
+        }
+        start = start + 1;
+        range = range + 1;
+
+        if (largestProduct < productSample) {
+            largestProduct = productSample;
+        }
+        productSample = 1;
     }
 }
-sumOfSquares(100)
 
-const squareOfSum = (n) => {
-    for (let i = 1; i <= n; i++) {
-        totalSquareOfSum += i;
-    }
-    totalSquareOfSum = totalSquareOfSum ** 2;
-}
-
-squareOfSum(100)
-
-let difference = totalSquareOfSum - totalSumOfSquares;
-console.log(difference)
+adjacentDigits(digitForTest)
+console.log(largestProduct)
