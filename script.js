46497b5984d52f533c70a5412c1e5be1232a0d60
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-03-28 17:53:58 +0600

    problem 27 solved

--- script.js
+++ script.js
@@ -475,33 +475,61 @@
 const digitForTest = `7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450`
 //* ---> Find the thirteen adjacent digits in the 1000 - digit number that have the greatest product.What is the value of this product ?
 
-let largestProduct = 1;
-let productSample = 1;
-let startingValue = 0;
-let range = 13;
+// let largestProduct = 1;
+// let productSample = 1;
+// let startingValue = 0;
+// let range = 13;
 
-const adjacentDigits = (n) => {
-    const array = n.toString().split("");
-    let stopCondition = array.length;
+// const adjacentDigits = (n) => {
+//     const array = n.toString().split("");
+//     let stopCondition = array.length;
 
-    for (let i = 1; i < stopCondition; i++) {
-        if (range >= array.length) {
-            stopCondition = 0;
-            range = 0;
+//     for (let i = 1; i < stopCondition; i++) {
+//         if (range >= array.length) {
+//             stopCondition = 0;
+//             range = 0;
+//         }
+//         for (let start = startingValue; start < range; start++) {
+//             productSample *= Number(array[start]);
+//         }
+//         startingValue = startingValue + 1;
+//         range = range + 1;
+//         console.log(largestProduct)
+//         if (largestProduct < productSample) {
+//             largestProduct = productSample;
+//         }
+//         console.log(largestProduct)
+//         productSample = 1;
+//     }
+// }
+
+// adjacentDigits(digitForTest)
+// console.log(largestProduct)
+
+//** ===================== Problem 27 : A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2. For example, 32 + 42 = 9 + 16 = 25 = 52. There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+let range = 1000;
+let arr = []
+let product;
+const onlyPythagoreanTriplet = () => {
+    for (let x = 1; x < 1000; x++) {
+        let a = x;
+        for (let y = 1; y < 1000; y++) {
+            let b = y;
+            for (let z = 1; z < 1000; z++) {
+                let c = z;
+                if (a ** 2 + b ** 2 === c ** 2 && a + b + c === 1000) {
+                    if (!arr.includes(a, b, c)) {
+                        arr = []
+                        arr.push(a, b, c)
+                    }
+                }
+            }
         }
-        for (let start = startingValue; start < range; start++) {
-            productSample *= Number(array[start]);
-        }
-        startingValue = startingValue + 1;
-        range = range + 1;
-        console.log(largestProduct)
-        if (largestProduct < productSample) {
-            largestProduct = productSample;
-        }
-        console.log(largestProduct)
-        productSample = 1;
     }
+    const [a, b, c] = arr
+    product = a * b * c;
 }
 
-adjacentDigits(digitForTest)
-console.log(largestProduct)
+onlyPythagoreanTriplet();
+console.log(product)
